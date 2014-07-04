5dd91740abdd67da94e500f3e36baeda8a6627d9
Prelang Builder <builder@prelang.com>
2014-07-04 16:10:04 +0000

    Defined validation for attribute 'email' on model 'Patient'

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,4 +2,5 @@ class Patient < ActiveRecord::Base
   belongs_to :doctor
   has_many :appointments
   validates_formatting_of :phone, using: :us_phone
+  validates_formatting_of :email, using: :email
 end
