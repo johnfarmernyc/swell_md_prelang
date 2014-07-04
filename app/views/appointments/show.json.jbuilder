52b6f3ab8ef6f394743a64c6ba6498876191b0f5
Prelang Builder <builder@prelang.com>
2014-07-04 16:10:18 +0000

    Generated scaffolding for model 'Appointment' via Rails

---
+++ app/views/appointments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @appointment, :id, :patient_id, :appt_time, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :appointments
+
   resources :doctors
 
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords"}, skip: [:sessions, :registrations]
