e2cde5013ef02ae44434d607b79f96e1b1ecf599
Prelang Builder <builder@prelang.com>
2014-07-04 16:10:03 +0000

    Defined validation for attribute 'phone' on model 'User'

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   has_many :doctors
+  validates_formatting_of :phone, using: :us_phone
 end
